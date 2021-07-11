fcc11be9f51a6873f6eecdde4cd9533476593dba
QambarALi1 <qamberali4904@gmail.com>
2021-07-11 09:54:19 +0500

    abc

--- app.js
+++ app.js
@@ -8,17 +8,40 @@
           // console.log(submit_form)
 
 // } 
-function matchpass(){
-          var text = document.f1.text.value;
-          var text = document.f1.text.value;
+// function matchpass(){
+//           var text = document.f1.text.value;
+//           var text = document.f1.text.value;
           
-          if(text==text){
+//           if(text==text){
 
-                    console.log((matchpass()))
-          return true;
-          }
-          else{
-          alert(matchpass());
-          return false;
-          }
-          }
+//                     console.log((matchpass()))
+//           return true;
+//           }
+//           else{
+//           alert(matchpass());
+//           return false;
+//           }
+//           }
+
+// document.getElementById("color");
+
+// onfocus = this.background
+
+// console.log(email);
+
+var txt = document.getElementById(txt).value;
+var email = document.getElementById(email).value;
+var password = document.getElementById(password).value;
+
+// alert(txt)
+// alert(email)
+// alert(password)
+
+function signup(){
+
+          alert(txt + email + password);
+          return ;
+}
+
+
+
