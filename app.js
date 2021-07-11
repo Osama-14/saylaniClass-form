cca668e4d95a0872bcde5671b8b34a1f7d8c0798
QambarALi1 <qamberali4904@gmail.com>
2021-07-11 09:30:22 +0500

    abc

---
+++ app.js
@@ -0,0 +1,24 @@
+// document.getElementById(submit)
+// document.getElementById(entered)
+
+// function submit_form(submit_form){
+//           if (entered === submit_form ){
+//                     console.log(submit);
+//           }
+          // console.log(submit_form)
+
+// } 
+function matchpass(){
+          var text = document.f1.text.value;
+          var text = document.f1.text.value;
+          
+          if(text==text){
+
+                    console.log((matchpass()))
+          return true;
+          }
+          else{
+          alert(matchpass());
+          return false;
+          }
+          }
